d2ebd82980f87d134943cba3921965283c9e4a8c
max <gsrivastava539@hotmail.com>
2022-08-04 23:59:19 -0400

    Remove Char from String

---
+++ DailyAssignments/RemoveCharFromString.java
@@ -0,0 +1,10 @@
+public class RemoveCharFromString {
+    public static void main(String[] args) {
+
+        String s="Appzlogic";
+
+        System.out.println("String after Removing 'p' = "+s.replace("p", ""));
+
+
+    }
+}
