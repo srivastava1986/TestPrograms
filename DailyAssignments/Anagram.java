18893d7d731687bf038362f865fec1cc2c32d4cb
max <gsrivastava539@hotmail.com>
2022-08-09 00:40:56 -0400

    Anagram

---
+++ DailyAssignments/Anagram.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String s1= "LISTEN";
+        String s2= "SILENT";
+        char[] c1= s1.toCharArray();
+        char[] c2= s2.toCharArray();
+        if (c1.length != c2.length)
+        {
+            System.out.println("Not Anagram");
+            System.exit(0);
+        }
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        for(int i=0; i< c1.length;i++)
+        {
+            if (c1.length != c2.length)
+            {
+                System.out.println("Not Anagram");
+                System.exit(0);
+            }
+        }
+        System.out.println("Anagram");
+    }
+}
