f3eae83b9292cd28fb72ddf1bcc0793541d43c3f
max <gsrivastava539@hotmail.com>
2022-08-09 23:55:04 -0400

    Frequency of Integers in Sorted Array

--- DailyAssignments/FrequencyofIntegersinSortedArray.java
+++ DailyAssignments/FrequencyofIntegersinSortedArray.java
@@ -5,13 +5,12 @@ public class FrequencyofIntegersinSortedArray {
         int arr[] = {10, 10, 20, 30, 30, 30};
         int N= arr.length;
         System.out.println(arr.length);
-        int freq=1;
+        int freq=0;
         for (int i=1; i<N; i++)
         {
             if (arr[i]==arr[i-1])
             {
                 freq++;
-
             }
         }
         System.out.println("Frequency of "+arr[N-1]+ " is: "+freq);
