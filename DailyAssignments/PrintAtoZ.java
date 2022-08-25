9187188fbec3304eb1b9097c2d253150f1ef13f2
max <gsrivastava539@hotmail.com>
2022-08-24 23:21:17 -0400

    Daily Tasks

---
+++ DailyAssignments/PrintAtoZ.java
@@ -0,0 +1,8 @@
+public class PrintAtoZ {
+    public static void main(String[] args) {
+        char ch;
+        for(ch='A'; ch<='Z';ch++){
+            System.out.print(ch+ " ");
+        }
+    }
+}
